b9d0cc72ab2c745b27ba5ca12d2cb786a7c76604
Acontplus S.A.S <zaratec@acontplus.com.ec>
2025-09-26 09:49:23 -0500

    uno

--- SriEload/background_ELOAD.js
+++
@@ -1,9 +0,0 @@
-var estadoActual = "...";
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'obtenerEstado') {
-    sendResponse({res: estadoActual});
-    return true; // Mantener el canal abierto para enviar la respuesta asincrónica
-  } else if (message.action === 'updateProgress') {
-    estadoActual = message.progress;
-  }
-});
